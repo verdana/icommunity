85b6834bb7e3bd507c302fc96d24183d5deec7ce
leeaee <leeaee@gmail.com>
2015-03-26 22:11:23 +0800

    精简状态设置

--- admin/src/main/java/com/modoop/admin/entity/Stateful.java
+++ admin/src/main/java/com/modoop/admin/entity/Stateful.java
@@ -11,34 +11,9 @@ public interface Stateful
     int NORMAL = 0;
 
     /**
-     * 状态：暂停前预警期
-     */
-    int WARNING = 1;
-
-    /**
-     * 状态：暂停
-     */
-    int PAUSED = 2;
-
-    /**
-     * 状态：过期
-     */
-    int EXPIRED = 3;
-
-    /**
-     * 状态：初始待审
-     */
-    int OPEN = 4;
-
-    /**
      * 状态：关闭
      */
-    int CLOSED = 5;
-
-    /**
-     * 未设置/不限制/所有
-     */
-    int NA = -1;
+    int CLOSED = 1;
 
     /**
      * 状态的 KEY
@@ -46,12 +21,7 @@ public interface Stateful
     String[] TEXT = new String[]
     {
         "正常",
-        "警告",
-        "暂停",
-        "过期",
-        "初始待审",
-        "关闭",
-        "无限制"
+        "关闭"
     };
 
     /**
