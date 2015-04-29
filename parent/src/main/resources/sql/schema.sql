72430c96f608bc2ebd62e07bf1f865c264da3f0c
jamesz008 <zx19819>
2015-04-29 22:53:05 +0800

    1: Add 'drop' before create table 2: correct error column to 'total_price' in house table

--- parent/src/main/resources/sql/schema.sql
+++ parent/src/main/resources/sql/schema.sql
@@ -5,6 +5,7 @@ USE modoop;
 /*==============================================================*/
 /* Table: admin                                                 */
 /*==============================================================*/
+DROP TABLE IF EXISTS `admin`;
 CREATE TABLE admin
 (
     id bigint AUTO_INCREMENT NOT NULL,
@@ -27,6 +28,7 @@ ENGINE = InnoDB ROW_FORMAT = DEFAULT CHARSET=utf8;
 /*==============================================================*/
 /* Table: role										                              */
 /*==============================================================*/
+DROP TABLE IF EXISTS `role`;
 CREATE TABLE role
 (
     id bigint AUTO_INCREMENT NOT NULL,
@@ -44,6 +46,7 @@ ENGINE = InnoDB ROW_FORMAT = DEFAULT CHARSET=utf8;
 /*==============================================================*/
 /* Table: permission								                            */
 /*==============================================================*/
+DROP TABLE IF EXISTS `permission`;
 CREATE TABLE permission
 (
     id bigint AUTO_INCREMENT NOT NULL,
@@ -58,6 +61,7 @@ ENGINE = InnoDB ROW_FORMAT = DEFAULT CHARSET=utf8;
 /*==============================================================*/
 /* Table: admin_x_role										                      */
 /*==============================================================*/
+DROP TABLE IF EXISTS `admin_x_role`;
 CREATE TABLE admin_x_role
 (
     admin_id bigint NOT NULL,
@@ -70,6 +74,7 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*==============================================================*/
 /* Table: saler                                                 */
 /*==============================================================*/
+DROP TABLE IF EXISTS `saler`;
 CREATE TABLE saler
 (
     id bigint AUTO_INCREMENT NOT NULL,
@@ -92,6 +97,7 @@ ENGINE = InnoDB ROW_FORMAT = DEFAULT CHARSET=utf8;
 /*==============================================================*/
 /* Table: broker                                                */
 /*==============================================================*/
+DROP TABLE IF EXISTS `broker`;
 CREATE TABLE broker
 (
     id bigint AUTO_INCREMENT NOT NULL,
@@ -113,6 +119,7 @@ ENGINE = InnoDB ROW_FORMAT = DEFAULT CHARSET=utf8;
 /*==============================================================*/
 /* Table: user                                                  */
 /*==============================================================*/
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE user
 (
     id bigint AUTO_INCREMENT NOT NULL,
@@ -137,6 +144,7 @@ ENGINE = InnoDB ROW_FORMAT = DEFAULT CHARSET=utf8;
 /*==============================================================*/
 /* Table: residential                                           */
 /*==============================================================*/
+DROP TABLE IF EXISTS `residential`;
 CREATE TABLE residential
 (
     id bigint AUTO_INCREMENT NOT NULL,
@@ -154,6 +162,7 @@ ENGINE = InnoDB ROW_FORMAT = DEFAULT CHARSET=utf8;
 /*==============================================================*/
 /* Table: house                                                 */
 /*==============================================================*/
+DROP TABLE IF EXISTS `house`;
 CREATE TABLE house
 (
     id bigint AUTO_INCREMENT NOT NULL,
@@ -161,7 +170,7 @@ CREATE TABLE house
     type tinyint(1) DEFAULT 0 NULL,
     area float NULL,
     price decimal(8,2) NULL,
-    totle_price decimal(11,2) NULL,
+    total_price decimal(11,2) NULL,
     discount float NULL,
     description varchar(255) DEFAULT NULL,
     create_time timestamp NOT NULL,
